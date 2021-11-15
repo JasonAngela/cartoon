560da3411954719dbf51b3562a1e53caf710fb15
huqilang <985104177@qq.com>
2021-11-15 23:41:39 +0800

    cartoon 自己玩的动漫集

---
+++ anyubao-api/src/test/java/cn/anyubao/passion/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.anyubao.passion.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
