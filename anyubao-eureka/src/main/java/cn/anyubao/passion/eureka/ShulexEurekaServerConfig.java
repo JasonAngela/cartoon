6823178553e9f662bef74f247fffee228627defd
huqilang <985104177@qq.com>
2021-11-25 16:01:48 +0800

    最新的eureka启动

---
+++ anyubao-eureka/src/main/java/cn/anyubao/passion/eureka/ShulexEurekaServerConfig.java
@@ -0,0 +1,12 @@
+package cn.anyubao.passion.eureka;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author huqilang
+ */
+@Configuration
+@EnableEurekaServer
+public class ShulexEurekaServerConfig {
+}
